8a3bbc52287553b34452fce9d753725523cdf6cf
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-05-19 16:06:20 +0200

    Automatically add every existent app/overrides in every plugin to Rails.application.paths

---
+++ app/overrides/README.txt
@@ -0,0 +1,1 @@
+This directory is only here to ensure the plugin works through unit tests. It is automatically added to Rails.application.paths["app/overrides"]
--- init.rb
+++ init.rb
@@ -6,3 +6,13 @@ Redmine::Plugin.register :redmine_base_deface do
   url 'https://github.com/jbbarth/redmine_base_deface'
   author_url 'jeanbaptiste.barth@gmail.com'
 end
+
+# Little hack for deface in redmine:
+# - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
+# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
+# - hack consists in adding "app/overrides" path of all plugins in Redmine's main #paths
+paths = Rails.application.paths["app/overrides"]
+paths ||= []
+Dir.glob("#{Rails.root}/plugins/*/app/overrides").each do |dir|
+  paths << dir unless paths.include?(dir)
+end
