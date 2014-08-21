3b61ec513b95c49877849aacd7f2bcef256c3778
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-21 19:02:44 +0200

    Add (commented) dependency on redmine_base_rspec for later

--- init.rb
+++ init.rb
@@ -5,6 +5,9 @@ Redmine::Plugin.register :redmine_base_deface do
   version '0.0.1'
   url 'https://github.com/jbbarth/redmine_base_deface'
   author_url 'jeanbaptiste.barth@gmail.com'
+  #doesn't work since redmine evaluates dependencies as it loads, and loads in lexical order
+  #TODO: see if it works in Redmine 2.6.x or 3.x when they're released
+  # requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
 end
 
 # Little hack for deface in redmine:
