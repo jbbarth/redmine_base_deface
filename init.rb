7ffa8fcb1364a0d22d5e219d0374942c946aec8f
Vincent Robert <vincent.robert@nanego.com>
2020-01-14 11:22:34 +0100

    Update Deface gem to latest version v1.5.3

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_base_deface do
   name 'Redmine Base Deface plugin'
   author 'Jean-Baptiste BARTH'
   description 'This is a plugin for Redmine'
-  version '1.3.2'
+  version '1.5.3'
   url 'https://github.com/jbbarth/redmine_base_deface'
   author_url 'jeanbaptiste.barth@gmail.com'
   #doesn't work since redmine evaluates dependencies as it loads, and loads in lexical order
