390bdf1141bba6ec949b330fdb69bf58e5dbd0d7
Liane Hampe <liaham@xmera.de>
2023-11-11 16:49:19 +0100

    Updates deface gem to 1.8.2
    Implements #1797

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_base_deface do
   name 'Redmine Base Deface plugin'
   author 'Jean-Baptiste BARTH'
   description 'This is a plugin for Redmine'
-  version '1.8.1-xmr-3'
+  version '1.8.2-xmr'
   url 'https://github.com/jbbarth/redmine_base_deface'
   author_url 'jeanbaptiste.barth@gmail.com'
   #doesn't work since redmine evaluates dependencies as it loads, and loads in lexical order
