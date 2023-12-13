0928db087449c2e722ccea1b4c62ffe2bef45fdc
David Lacerte <7608394+SolarBear@users.noreply.github.com>
2023-12-13 11:29:38 -0500

    Fix using classic autoloader in Rails 6.x
    While Rails up to 5 will use the classic autoloader and Rails 7+ will use zeitwerk, Rails 6.x can still be configured to use the classic autoloader: in that case, the code with throw an exception as it expects the latter behavior.
    
    By adding a call to `zeitwerk_enabled?`, we support both cases, while having no impact on other Rails versions.
    
    See [Rails 7's](https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#rails-autoloaders) and [Rails 6's](https://guides.rubyonrails.org/v6.0/autoloading_and_reloading_constants.html#opting-out) docs for more information.

--- init.rb
+++ init.rb
@@ -14,7 +14,11 @@ end
 # - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
 # - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
 # - hack consists in adding "app/overrides" path of all plugins in Redmine's main #paths
-if Rails.version > '6.0'
+
+# Select the autoloader to use: zeitwerk or the classic one
+# The `zeitwerk_enabled?` predicate returns `true` for Rails >= 7
+# For Rails 6.x, it returns `Rails.configuration.autoloader == :zeitwerk`
+if Rails.version > '6.0' && Rails.autoloaders.zeitwerk_enabled?
   Dir.glob("#{Rails.root}/plugins/*/app/overrides/**/*.rb").each do |path|
     Rails.autoloaders.main.ignore(path)
     load File.expand_path(path, __FILE__)
