46dacf3e0a3d8a8c96136a9f1f0cbaa31ae539c8
Vincent <contact@vincent-robert.com>
2025-06-24 11:48:54 +0200

    Ensure compatibility with latest Redmine versions

--- init.rb
+++ init.rb
@@ -5,16 +5,25 @@ Redmine::Plugin.register :redmine_base_deface do
   version '6.0.1'
   url 'https://github.com/jbbarth/redmine_base_deface'
   author_url 'jeanbaptiste.barth@gmail.com'
-  #doesn't work since redmine evaluates dependencies as it loads, and loads in lexical order
-  #TODO: see if it works in Redmine 2.6.x or 3.x when they're released
+  # doesn't work since redmine evaluates dependencies as it loads, and loads in lexical order
+  # TODO: see if it works in Redmine 2.6.x or 3.x when they're released
   # requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
 end
 
-# Little hack for deface in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of all plugins in Redmine's main #paths
-if Rails.version > '6.0'
+if Rails::VERSION::MAJOR >= 7
+  Dir.glob("#{Rails.root}/plugins/*/app/overrides/**/*.rb").each do |path|
+    Rails.autoloaders.main.ignore(path)
+    load File.expand_path(path, __FILE__)
+  end
+
+  Dir.glob("#{Rails.root}/plugins/*/app/overrides/**/*.deface").each do |path|
+    Deface::DSL::Loader::load File.expand_path(path, __FILE__)
+  end
+
+  Rails.application.config.after_initialize do
+    require_relative "lib/applicator_patch"
+  end
+elsif Rails::VERSION::MAJOR == 6
   Rails.application.config.after_initialize do
     Dir.glob("#{Rails.root}/plugins/*/app/overrides/**/*.rb").each do |path|
       Rails.autoloaders.main.ignore(path)
@@ -28,6 +37,10 @@ if Rails.version > '6.0'
     require_relative "lib/applicator_patch"
   end
 else
+  # Little hack for deface in redmine:
+  # - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
+  # - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
+  # - hack consists in adding "app/overrides" path of all plugins in Redmine's main #paths
   Rails.application.paths["app/overrides"] ||= []
   Dir.glob("#{Rails.root}/plugins/*/app/overrides").each do |dir|
     Rails.application.paths["app/overrides"] << dir unless Rails.application.paths["app/overrides"].include?(dir)
